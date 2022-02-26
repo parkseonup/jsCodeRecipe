5ccd992b2c12e27967173fd2db1765861f5fd437
Seonu <seonup95@gmail.com>
2022-02-27 00:27:05 +0900

    Add Comment

--- chapter2/031/app.js
+++ chapter2/031/app.js
@@ -5,27 +5,28 @@ const searchWordText = document.querySelector('#search-word-input');
 const prefectureList = document.querySelectorAll('#prefecture-list');
 const prefectureBtns = document.querySelectorAll('#prefecture-list > button');
 
-const nameArray = {};
-const phoneticArray = {};
+const prefectureArray = [];
 
 function searchWordFunc() {
 
   prefectureBtns.forEach((btn) => {
-    // data-name의 첫글자와 input 값이 같으면 nameArray
-    console.log(btn.dataset.name.charAt(1), searchWordText.value);
-    if (btn.dataset.name.charAt(1) === searchWordText.value) {
-      console.log('hello');
-      //nameArray.push(btn);
-    }
-    
-    // data-phonetic의 첫글자와 input 값이 같으면 phoneticArray
-    if (btn.dataset.phonetic.charAt(1) === searchWordText.value) {
-      console.log(btn.dataset.phonetic.charAt(1), searchWordText.value);
-      console.log('world');
-      //phoneticArray.push(btn);
+    switch (searchWordText.value) {
+      case btn.dataset.name.charAt(0):
+        console.log('hello');
+        prefectureArray.push(btn);
+        break;
+      case btn.dataset.phonetic.charAt(0):
+        console.log('world');
+        prefectureArray.push(btn);
+        break;
+      default:
+        // node에는 뜨는데 html에는 안뜲
+        prefectureList.innerText = '일치하는 검색어가 없습니다.';
+        break;
     }
 
-    // a와 b를 리스트에 띄워라
+    // node에는 뜨는데 html에는 안뜲
+    prefectureList.innerHTML = prefectureArray;
   });
 }
 
